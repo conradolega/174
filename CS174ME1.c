a80ba9b1e644c3f5acbb05417603fd67a09f696c
conradolega <conradolega@gmail.com>
2014-01-03 15:14:01 +0800

    Add semaphore and threads

--- CS174ME1.c
+++ CS174ME1.c
@@ -3,6 +3,13 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+#define NUM_THREADS 5
+
 int main() {
+	int i = 0;
+	pthread_t philosophers[NUM_THREADS];
+	sem_t sem;
+
+	sem_init(&sem, 0, 1);
 	pthread_exit(NULL);
 }
