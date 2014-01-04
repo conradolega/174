692fd53b57cd1d54166d45e8abf13f2d0fc5aad9
conradolega <conradolega@gmail.com>
2014-01-04 13:26:01 +0800

    Add data structure containing data for each thread

--- CS174ME1.c
+++ CS174ME1.c
@@ -5,11 +5,23 @@
 
 #define NUM_THREADS 5
 
+typedef struct {
+	int rank;
+	sem_t *sem;
+} philo_t;
+
 int main() {
 	int i = 0;
 	pthread_t philosophers[NUM_THREADS];
+	philo_t *philo_data[NUM_THREADS];
 	sem_t sem;
 
 	sem_init(&sem, 0, 1);
+	for (i = 0; i < NUM_THREADS; i++) {
+		philo_data[i] = (philo_t *) malloc(sizeof(philo_t));
+		philo_data[i]->rank = i;
+		philo_data[i]->sem = &sem;
+	}
+
 	pthread_exit(NULL);
 }
