8ee28bb3701ff8174bfcd5a2394f3735ed9d9313
conradolega <conradolega@gmail.com>
2014-01-05 23:06:51 +0800

    Use multple semaphores, one for each fork

--- CS174ME1.c
+++ CS174ME1.c
@@ -7,33 +7,32 @@
 
 typedef struct {
 	int rank;
-	int *left_fork;
-	int *right_fork;
-	sem_t *sem;
+	sem_t *left_fork;
+	sem_t *right_fork;
 } philo_t;
 
 void *philosopher(void *philo_data) {
 	philo_t *data = (philo_t *) philo_data;
 	while (1) {
-		sem_wait(data->sem);
-		printf("Philosopher %d has the semaphore.\n", data->rank);
-		sem_post(data->sem);
+		sem_wait(data->left_fork);
+		sem_wait(data->right_fork);
+		sem_post(data->left_fork);
+		sem_post(data->right_fork);
 	}
 	pthread_exit(NULL);
 }
 
 int main() {
 	int i = 0;
-	int forks[NUM_THREADS] = {0, 0, 0, 0, 0};
+	sem_t forks[NUM_THREADS];
 	pthread_t philosophers[NUM_THREADS];
 	philo_t *philo_data[NUM_THREADS];
-	sem_t sem;
 
-	sem_init(&sem, 0, 1);
+	for (i = 0; i < NUM_THREADS; i++) sem_init(&forks[i], 0, 1);
+
 	for (i = 0; i < NUM_THREADS; i++) {
 		philo_data[i] = (philo_t *) malloc(sizeof(philo_t));
 		philo_data[i]->rank = i;
-		philo_data[i]->sem = &sem;
 		if (i - 1 < 0) philo_data[i]->left_fork = &forks[NUM_THREADS - 1];
 		else philo_data[i]->left_fork = &forks[i - 1];
 		philo_data[i]->right_fork = &forks[i];
