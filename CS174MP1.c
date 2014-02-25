28605db17499a7630e44f91f2a369dd1c6131162
conradolega <conradolega@gmail.com>
2014-02-25 22:36:06 +0800

    Add barrier init function

--- CS174MP1.c
+++ CS174MP1.c
@@ -9,6 +9,14 @@ typedef struct {
 	int counter;
 } barrier_t;
 
+void barrier_init(barrier_t *barrier, int threads) {
+	barrier->mutex = (sem_t *) malloc(sizeof(sem_t));
+	barrier->sem = (sem_t *) malloc(sizeof(sem_t));
+	sem_init(barrier->mutex, 0, 1);
+	sem_init(barrier->sem, 0, 0);
+	barrier->counter = 0;
+}
+
 int main(int argc, char* argv[]) {
 
 }
